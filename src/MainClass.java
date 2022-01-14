fe15a4813f70d85a98e7a34c6c5c5e694b85001d
Vinit <Vinitrana5437@gmail.com>
2022-01-14 14:56:24 +0530

    Registration Form

---
+++ src/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ *
+ * @author VINIT RANA
+ */
+public class MainClass {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Registration Portal");
+       MyFrame W;
+        W = new MyFrame();
+    }
+    
+}
